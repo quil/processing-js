952a5faa9203c6465bebd8c94c498153b4d76eb2
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-08-08 21:01:43 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,3 @@
 (defproject quil/processing-js "1.4.8"
   :description "Container of Processing.js for ClojureScript"
-  :url "https://github.com/Norgat/quil-processing-js.git"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"})
+  :url "https://github.com/quil/processing-js")
